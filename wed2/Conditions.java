c32a565645dee99fdae90e54963157aa9ae21b26
Krishna Kumar <bkkrishnakumar12@gmail.com>
2023-08-24 20:08:48 +0530

    23-8-23 Task 2

---
+++ wed2/Conditions.java
@@ -0,0 +1,27 @@
+package org.wed2;
+
+import java.util.Scanner;
+
+public class Conditions {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+
+		int s = sc.nextInt();
+		System.out.println("Enter the number"+ s);
+		
+		int add = sc.nextInt();
+		System.out.println(add);
+		
+		int sub = sc.nextInt();
+		System.out.println(sub);
+		
+		int mul = sc.nextInt();
+		System.out.println(mul);
+		
+		float div = sc.nextFloat();
+		System.out.println(div);
+		
+	}
+
+}
